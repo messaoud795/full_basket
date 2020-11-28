ab5eca1685c12f6e9531241466e7118fe2318499
messaoud795 <messaoudnabil9@gmail.com>
2020-11-28 16:44:03 +0100

    design2

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema=mongoose.Schema({
     firstName:{ type : String, required :true},
     lastName:{ type : String, required :true},
     address:{ type : String, required :true},
-    email: { type : String, required :true, unique:true},
+    email: { type : String, required :true, unique:true,lowercase: true},
     password : { type : String, required :true},
     isAdmin: {type : Boolean, default: false, required:true},
     basketId: {type:mongoose.Schema.Types.ObjectId, ref:'basket'},
